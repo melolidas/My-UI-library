39c79b938aac8d1b478c6bd56b7d71dd1a9b8b16
theHalid <the_halidcastle@mail.ru>
2024-11-22 15:22:27 +0600

    intermediate commit

--- dist/script.js
+++ dist/script.js
@@ -24,11 +24,6 @@ $.prototype.init = function (selector) {
     this.length = 1;
     return this;
   }
-  if (selector.tagName) {
-    this[0] = selector;
-    this.length = 1;
-    return this;
-  }
   Object.assign(this, document.querySelectorAll(selector));
   this.length = document.querySelectorAll(selector).length;
   return this;
